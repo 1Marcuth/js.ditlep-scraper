18ced94596d48feb9caddf5b69db3a6c9ae67898
TantoFaz <thiago.playgames15@gmail.com>
2022-11-29 23:49:11 -0300

    correção do bug das rotas que considerava obrigatório passar o mês

--- src/server/routers/ditlep-crawler.js
+++ src/server/routers/ditlep-crawler.js
@@ -5,6 +5,6 @@ import controllers from "../controllers/index.js";
 const ditlepCrawler = controllers.ditlepCrawler;
 const router = Router();
 
-router.get("/ditlep/alliance-chests/:month", ditlepCrawler.allianceChests);
+router.get("/ditlep/alliance-chests/:month?", ditlepCrawler.allianceChests);
 
 export default router;
